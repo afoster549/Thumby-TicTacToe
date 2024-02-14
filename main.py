afe4e5c6299bb399ff9cc6579c9ce726bb04b798
Adam Foster <100795364+afoster549@users.noreply.github.com>
2024-02-14 18:20:08 +0000

    Update main.py
    Added mode selection
    Added multiplayer support
    Added player indicator

--- main.py
+++ main.py
@@ -23,9 +23,15 @@ activeCell = {
     "yOld": 0
 }
 
-currentPlayer = "x"
+player = "x"
 
-playing = True
+multiplayer = False
+host = False
+playing = False
+
+end = False
+draw = False
+winner = ""
 
 def drawBoard():
     thumby.display.fill(0)
@@ -37,11 +43,78 @@ def drawBoard():
     
     thumby.display.drawFilledRectangle(22, 6, 9, 9, 1)
     
+    thumby.display.drawText(player, 0, 0, 1)
+    
     thumby.display.update()
+
+def waitForPlayer():
+    global player
+    global playing
+    
+    while True:
+        recived = thumby.link.receive()
+        
+        if recived != None:
+            if recived[1] == 0:
+                player = "o"
+                
+                host = False
+                
+                thumby.link.send(bytearray([1, 1]))
+            else:
+                playing = True
+                
+            break
+        else:
+            thumby.link.send(bytearray([1, 0]))
+            
+            host = True
+
+def modeSelect():
+    global multiplayer
+    global playing
+    
+    thumby.display.fill(0)
+    
+    thumby.display.drawText("Mode Select", 2, 5, 1)
+    thumby.display.drawText("A: 1 player", 2, 21, 1)
+    thumby.display.drawText("B: 2 player", 2, 30, 1)
+    
+    thumby.display.update()
+    
+    while True:
+        if thumby.buttonA.justPressed():
+            print("1 player")
+            
+            multiplayer = False
+            playing = True
+            
+            break
+        elif thumby.buttonB.justPressed():
+            multiplayer = True
+            
+            thumby.display.fill(0)
+            thumby.display.drawText("Waiting...", 8, 18, 1)
+            
+            thumby.display.update()
+            
+            waitForPlayer()
+            
+            break
+        
+    drawBoard()
 
 def hideCursor():
     global playing
+    global draw
+    global end
+    global winner
+    
+    if playing and not draw:
+        winner = player
+    
     playing = False
+    end = True
     
     cell = board[activeCell["y"]][activeCell["x"]]
     
@@ -57,8 +130,8 @@ def hideCursor():
         thumby.display.drawSprite(cellSprite)
 
 def checkWin():
-    global currentPlayer
-    draw = True
+    global player
+    global draw
     
     for i in range(0, 3):
         if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != "":
@@ -86,34 +159,42 @@ def checkWin():
         thumby.display.drawLine(22, 34, 50, 6, 1)
         
         return
+    
+    draw = True
         
     for row in board:
         for cell in row:
             if cell == "":
                 draw = False
-                
+    
     if draw == True:
-        currentPlayer = ""
-        
         hideCursor()
 
-def place():
-    global currentPlayer
+def place(player, x, y):
+    board[y][x] = player
     
-    board[activeCell["y"]][activeCell["x"]] = currentPlayer
-    
-    if currentPlayer == "x":
-        cellSprite = thumby.Sprite(9, 9, bitmapXSelected, 22 + (activeCell["x"] * 10), 6 + (activeCell["y"] * 10))
+    if player == "x":
+        bmp = None
+        
+        if activeCell["x"] == x and activeCell["y"] == y:
+            bmp = bitmapXSelected
+        else:
+            bmp = bitmapX
+        
+        cellSprite = thumby.Sprite(9, 9, bmp, 22 + (x * 10), 6 + (y * 10))
         
         thumby.display.drawSprite(cellSprite)
-        
-        currentPlayer = "o"
     else:
-        cellSprite = thumby.Sprite(9, 9, bitmapOSelected, 22 + (activeCell["x"] * 10), 6 + (activeCell["y"] * 10))
+        bmp = None
+        
+        if activeCell["x"] == x and activeCell["y"] == y:
+            bmp = bitmapOSelected
+        else:
+            bmp = bitmapO
+        
+        cellSprite = thumby.Sprite(9, 9, bmp, 22 + (x * 10), 6 + (y * 10))
         
         thumby.display.drawSprite(cellSprite)
-        
-        currentPlayer = "x"
     
     checkWin()
     
@@ -148,6 +229,10 @@ def update():
     thumby.display.update()
 
 def checkInput():
+    global player
+    global playing
+    global multiplayer
+    
     if thumby.buttonU.justPressed():
         if activeCell["y"] - 1 >= 0:
             activeCell["xOld"] = activeCell["x"]
@@ -180,26 +265,69 @@ def checkInput():
             activeCell["x"] += 1
             
             update()
-    elif thumby.buttonA.justPressed():
+    elif thumby.buttonA.justPressed() and playing:
         if board[activeCell["y"]][activeCell["x"]] == "":
-            place()
+            place(player, activeCell["x"], activeCell["y"])
+            
+            if multiplayer:
+                playing = False
+                
+                thumby.link.send(bytearray(
+                    [
+                        0,
+                        activeCell["x"],
+                        activeCell["y"]
+                    ]
+                ))
+            else:
+                thumby.display.drawText(player, 0, 0, 0)
+                
+                if player == "x":
+                    player = "o"
+                else:
+                    player = "x"
+                
+                thumby.display.drawText(player, 0, 0, 1)
+    
+                thumby.display.update()
+            
+def checkResponse():
+    global playing
+    
+    recived = thumby.link.receive()
+    
+    if recived != None and recived[0] == 0:
+        if player == "x":
+            playing = True
+            
+            place("o", recived[1], recived[2])
+        else:
+            playing = True
+            
+            place("x", recived[1], recived[2])
 
 def endGame():
     global board
     global activeCell
-    global currentPlayer
+    
+    global player
+    
+    global multiplayer
+    global host
     global playing
+    
+    global end
+    global draw
+    global winner
     
     time.sleep(2)
 
     thumby.display.fill(0)
         
-    if currentPlayer == "x":
-        thumby.display.drawText("O Won!", 20, 10, 1)
-    elif currentPlayer == "o":
-        thumby.display.drawText("X Won!", 20, 10, 1)
-    else:
+    if draw:
         thumby.display.drawText("Draw!", 23, 10, 1)
+    else:
+        thumby.display.drawText(f"{winner} Won!", 20, 10, 1)
             
     thumby.display.drawText("A. New Game", 4, 20, 1)
     thumby.display.drawText("B. Exit", 4, 30, 1)
@@ -222,19 +350,31 @@ def endGame():
                 "xOld": 0,
                 "yOld": 0
             }
-                
-            currentPlayer = "x"
-                
+            
             playing = True
+            
+            player = "x"
+    
+            multiplayer = False
+            host = False
+            playing = False
+            
+            end = False
+            draw = False
+            winner = ""
+            
+            modeSelect()
             
             break
         elif thumby.buttonB.justPressed():
             thumby.reset()
 
-drawBoard()
+modeSelect()
 
 while True:
-    if playing:
-        checkInput()
-    else:
+    checkInput()
+        
+    checkResponse()
+    
+    if end:
         endGame()
